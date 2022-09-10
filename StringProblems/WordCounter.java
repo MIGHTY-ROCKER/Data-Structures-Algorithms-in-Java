fee7917cf0a5db19ff565314646cc1f0ba529db5
Er. Ajay Prakash Kalyanasundaram <104113272+ajay-k-sundaram@users.noreply.github.com>
2022-09-11 04:44:24 +0530

    Count the words in the given string

---
+++ StringProblems/WordCounter.java
@@ -0,0 +1,23 @@
+// This program will count the number of words in a sentence depends on the space " " between each words.
+// Input: "I am not in Danger Skyler, I am the Danger"
+// Output: 10
+
+
+import java.util.Scanner;
+public class MyClass {
+    static void count(String str){
+        char[] a = str.toCharArray();
+        int count = 1 ;
+        for(int i = 0; i<a.length; i++){
+           if(a[i] == ' '){
+               count++;
+           }
+        }
+        System.out.print(count + " ");
+    }
+    public static void main(String args[]) {
+     Scanner input = new Scanner(System.in);
+     String str = input.nextLine();
+     count(str);
+    }
+}
