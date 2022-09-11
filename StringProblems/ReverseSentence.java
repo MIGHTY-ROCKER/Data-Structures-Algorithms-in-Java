5e563790ce4fe072834a852a55fe3c19e2874145
Er. Ajay Prakash Kalyanasundaram <104113272+ajay-k-sundaram@users.noreply.github.com>
2022-09-11 08:12:42 +0530

    Reverse the given Input String

--- StringProblems/ReverseSentence.java
+++ StringProblems/ReverseSentence.java
@@ -24,3 +24,41 @@ public class ReverseSentence {
     Package Names: java.util.regex.Pattern;
     java.util.stream.Collectors
 */
+
+/* Alternate Approach
+
+public class ReverseSentence {
+    public static String reverseWords (String s) { 
+        String result = new String();
+        int i = 0;
+        int n = s.length();
+
+        while(i < n){
+
+            while(i < n && s.charAt(i) == ' ') i++;
+
+            if(i >= n) break;
+
+            int j = i + 1;
+
+            while(j < n && s.charAt(j) != ' '){
+                j++;
+            }  
+            String sub = s.substring(i, j);
+
+            if(result.length() == 0) result = sub;
+            else result = sub + " " + result;
+
+            i = j + 1;
+
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        String s = "  How are you  ";
+        System.out.println(reverseWords(s));
+    }
+}
+
+// Time Complexity O(n^2)
+*/
